b04a8cf717b3f02fb5699e478c405d64b50fb0ca
Jacuzzi <jheklayag@gmail.com>
2025-03-07 12:42:32 +0800

    Batch 1 Added

---
+++ Prog04 - Product.py
@@ -0,0 +1,6 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+product = a * b
+
+print(product)
---
+++ Prog07 - Sum of 10 Numbers.py
@@ -0,0 +1,5 @@
+numbers = [int(input("Enter a number " + str(i+1) + ": ")) for i in range(10)]
+
+total = sum(numbers)
+
+print(total)
